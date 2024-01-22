58aa96cf9f068b36f83df0f02a25e995d8ca7061
Mehmet Furkan <mfturkcan@gmail.com>
2024-01-23 00:42:59 +0300

    Remove availbility

--- internal/store/migrations/20240122213222_update_restaurant_time.sql
+++ internal/store/migrations/20240122213222_update_restaurant_time.sql
@@ -10,7 +10,6 @@ USING TO_CHAR(weekend_available_at_start, 'HH12:MM');
 ALTER COLUMN weekend_available_at_end TYPE TEXT
 USING TO_CHAR(weekend_available_at_end, 'HH12:MM');
 
-ALTER TABLE restaurant
 DROP COLUMN is_available;
 -- +goose StatementEnd
 
